43363a03897912f342ac490d8dbc8faf3702fdf3
姜鹄 <1160321430@qq.com>
2018-01-16 14:20:55 +0800

    expand small sh: support input line parse, if then else flowcontrol, global var

---
+++ sh-further/smsh4/smsh4.c
@@ -0,0 +1,49 @@
+/* smsh3.c -- small shell version 2
+ * small shell that supports command line parsing
+ * and if..then..else..fi logic(by calling process())
+ * compile: cc smsh4.c splitline.c execute.c process.c controlflow.c builtin.c varlib.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include "smsh.h"
+
+#define DEL_PROMPT "> "
+
+int main() {
+    char* cmdline, * prompt, ** arglist; 
+    int result, process(char**);
+    void setup();
+
+    prompt = DEL_PROMPT;
+    setup();
+
+    while ((cmdline = next_cmd(prompt, stdin)) != NULL) {
+        if ((arglist = splitline(cmdline)) != NULL) {
+            result = process(arglist);
+            freelist(arglist);
+        }
+        free(cmdline);
+    }
+    return 0;
+}
+
+/* 
+ * purpose: initialize shell
+ * returns: nothing calls fatal() if trouble
+ */
+void setup() {
+    extern char** environ;
+    
+    VLenviron2table(environ);
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+}
+
+void fatal(char* s1, char* s2, int n) {
+    fprintf(stderr, "Error: %s, %s\n", s1, s2);
+    exit(n);
+}
