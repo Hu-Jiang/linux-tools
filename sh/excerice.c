9c87dd30b2dbfa815d3a905f9429ba5f452300b8
姜鹄 <1160321430@qq.com>
2018-01-16 14:18:17 +0800

    implement a small sh

---
+++ sh/excerice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    if (fork() != 0) {
+        exit(0);
+    }
+
+    for (i = 1; i <= 10; i++) {
+        printf("still here. \n");
+        sleep(i);
+    }
+    return 0;
+}
