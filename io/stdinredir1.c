86a262c5a7b24ded40297e3036b8450853ef8575
姜鹄 <1160321430@qq.com>
2018-01-17 00:31:42 +0800

    change IO to io

---
+++ io/stdinredir1.c
@@ -0,0 +1,33 @@
+/* stdinredir1.c
+ * purpose: show how to redirect standard input by replacing file
+ *          descriptor 0 with a connection to a file.
+ * action: reads three lines from standard input, then close fd 0,
+ *         open a disk file, then reads in three more lines from 
+ *         standard input
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+
+void main() {
+    int fd;
+    char line[100];
+
+    // read and print three lines
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+
+    // redirect input
+    close(0);
+    fd = open("/etc/passwd", O_RDONLY);
+    if (fd != 0) {
+        fprintf(stderr, "Cound not open data as fd 0\n");
+        exit(1);
+    }    
+
+    // read and print three lines
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);    
+}
